2e30ad3574877f8b405814bb03e8f564e1f28488
Yllan <the_yllan@hotmail.com>
2022-01-15 13:36:40 -0300

    implementando usecase de listagem de usuários

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,14 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { id } = request.body;
+
+    const users = this.listAllUsersUseCase.execute(id);
+
+    return response.json(users);
   }
 }
 
