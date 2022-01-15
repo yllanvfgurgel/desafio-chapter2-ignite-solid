b6f103d58f61b135b7a18c0d07bde6c78da6bb37
Yllan <the_yllan@hotmail.com>
2022-01-15 13:42:33 -0300

    implementando usecase de detalhes de usuário

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,10 +3,14 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { id } = request.params;
+
+    const user = this.showUserProfileUseCase.execute({ user_id: id });
+
+    return response.json(user);
   }
 }
 
